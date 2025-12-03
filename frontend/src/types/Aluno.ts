9df2823f2ee63db9bf88b624ec16109d5458abc7
Manoelpj <manoelpinto2005@gmail.com>
2025-12-03 10:01:02 -0300

    feat: criar tipo aluno

---
+++ frontend/src/types/Aluno.ts
@@ -0,0 +1,10 @@
+export interface Aluno {
+  id: number;
+  nome: string;
+  endereco: string;
+  responsavelNome: string;
+  turno: string;
+  tipo: string;
+  formaPagamento: string;
+  escola: string;
+}
