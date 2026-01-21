2280abd7b0aabe7e7511be70c36aefb1677b78ad
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2026-01-21 11:44:46 +0000

    feat: adicionar urls dos tokens jwt

--- backend/escolar/escolar/urls.py
+++ backend/escolar/escolar/urls.py
@@ -23,6 +23,10 @@ from rest_framework.routers import DefaultRouter
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from api import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -42,4 +46,6 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
